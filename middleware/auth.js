42ec4391ad4f11ebb9711081c53faa5514dde486
Sonam-git15 <sonamrai5180@gmail.com>
2025-04-18 15:37:55 +0530

    Initial backend commit

---
+++ middleware/auth.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const verifyToken = async (req, res, next) =>{
+
+    let token
+
+    if (
+        req.headers.authorization && req.headers.authorization.startsWith('Bearer')
+    ){
+
+        try{
+    
+            token = req.headers.authorization.split(' ')[1];
+
+            const verified = jwt.verify(token, process.env.JWT_SECRET);
+            
+            // Get user from token
+            req.user = await User.findById(verified.id).select("-password");
+            next();
+    
+        }catch(err){
+            res.status(500).json({error: err.message});
+        }
+    }
+
+    if(!token){
+        return res.status(403);
+        throw new Error("Access Denied");
+    }
+   
+}
